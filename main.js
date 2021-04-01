928abe9784e72073c13bf2b9f30498a01b2e946d
Bianca <biancancanal@gmail.com>
2021-04-01 11:07:24 -0500

    commit tree 62ac020707e2e857a9c67ee529c8ef2d843c8717

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
+
+
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+    mimicServerCall();
+})
 
 
 
+function likeButton(event) {
+  console.log(event.target)
+  let error = document.getElementByID("modal-message")
+  error.hidden = true
+}
+
+
+// fetch("http://mimicServer.example.com")
+// .then(resp => resp.json())
+// .then(data => {
+//   console.log(data)
+//   let error = document.getElementByID("modal-message")
+//   error.hidden = true
+//   document
+// })
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
